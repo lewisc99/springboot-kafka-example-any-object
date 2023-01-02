ae4275cef0cda93b9983e38206899c717872da59
lewisc99 <lewiscontato99@gmail.com>
2023-01-02 09:24:34 -0300

    added consumer kafka-dead letter and listener dead-letter

--- kafka-consumer/src/main/java/com/lewis/kafkaconsumer/config/ConsumerKafkaConfig.java
+++ kafka-consumer/src/main/java/com/lewis/kafkaconsumer/config/ConsumerKafkaConfig.java
@@ -1,17 +1,25 @@
 package com.lewis.kafkaconsumer.config;
 
 
+import com.fasterxml.jackson.databind.JsonSerializable;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
-import org.springframework.kafka.core.ConsumerFactory;
-import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.core.*;
+import org.springframework.kafka.listener.CommonErrorHandler;
+import org.springframework.kafka.listener.DeadLetterPublishingRecoverer;
+import org.springframework.kafka.listener.DefaultErrorHandler;
 import org.springframework.kafka.support.converter.JsonMessageConverter;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+import org.springframework.util.backoff.FixedBackOff;
 
 import java.util.HashMap;
 
@@ -31,7 +39,11 @@ public class ConsumerKafkaConfig {
         configs.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         configs.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
 
-        return new DefaultKafkaConsumerFactory<>(configs);
+
+        var jsonDeserializer = new JsonDeserializer(Object.class).trustedPackages("*")
+                .forKeys();
+
+        return new DefaultKafkaConsumerFactory<>(configs, new StringDeserializer(), jsonDeserializer);
 
     }
 
@@ -42,6 +54,24 @@ public class ConsumerKafkaConfig {
         var factory = new ConcurrentKafkaListenerContainerFactory();
         factory.setConsumerFactory(jsonConsumerFactory());
         factory.setMessageConverter(new JsonMessageConverter());
+        factory.setCommonErrorHandler(defaultErrorHandler());
         return factory;
     }
+
+    private CommonErrorHandler defaultErrorHandler() {
+        var recoverer = new DeadLetterPublishingRecoverer(new KafkaTemplate<>(deadLetterFactory()));
+        return new DefaultErrorHandler(recoverer, new FixedBackOff(100L,2));
+    }
+
+    private ProducerFactory<String, Object> deadLetterFactory() {
+        var configs = new HashMap<String,Object>();
+        configs.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaProperties.getBootstrapServers());
+        configs.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configs.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializable.class);
+
+        return new DefaultKafkaProducerFactory<>(configs, new StringSerializer(), new JsonSerializer<>());
+    }
+
+
+
 }
--- kafka-consumer/src/main/java/com/lewis/kafkaconsumer/listener/TestListener.java
+++ kafka-consumer/src/main/java/com/lewis/kafkaconsumer/listener/TestListener.java
@@ -15,10 +15,29 @@ public class TestListener {
     public void personExample(Person person, ConsumerRecordMetadata metadata)
     {
         System.out.println("Topic : " + metadata.topic() + " partition: "
-        + metadata.partition());
+        + metadata.partition() + " groupId: group-1 ");
         System.out.println(" Person name : " + person.getName() + " Person Age: " + person.getAge());
         System.out.println("-------------------------------------------");
 
     }
 
+
+     @KafkaListener(topics = "person-example-topic", groupId = "group-2", containerFactory = "jsonKafkaListenerContainerFactory")
+    public void personExampleDifferentGroupId(Person person, ConsumerRecordMetadata metadata)
+    {
+        System.out.println("Topic : " + metadata.topic() + " partition: "
+                + metadata.partition() + " groupId: group-2 ");
+        System.out.println(" Person name : " + person.getName() + " Person Age: " + person.getAge());
+        System.out.println("-------------------------------------------");
+
+    }
+
+    @KafkaListener(groupId = "group-2", topics = "person-example-DLT")
+    public void personExampleDLT(Person person, ConsumerRecordMetadata metadata)
+    {
+
+        System.out.println("Topic: " + metadata.topic());
+        System.out.println("Person example DLT: " + person.getName() + " Person age: " + person.getAge());
+        System.out.println("-------------------------------------------");
+    }
 }
